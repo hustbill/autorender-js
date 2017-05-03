33225210a0a580120e00cd1aefa7ca0085bc8913
hua zhang <hua.zhang1@huawwei.com>
2017-05-02 18:05:22 -0700

    add local.server.js

--- app/scripts/components/nodes-resources.js
+++ app/scripts/components/nodes-resources.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import LineChart from 'react-linechart';
+import BarChart from 'react-bar-chart';
+import '../../styles/BarChart.css';
 import '../../styles/styles.css';
 
 import Logo from './logo';
@@ -12,38 +14,74 @@ import {
 } from '../actions/app-actions';
 
 class NodesResources extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+  }
+
+  handleMouseEnter(ev) {
+    ev.preventDefault();
+    ev.stopPropagation();
+    this.props.enterEdge(ev.currentTarget.id);
+    console.log(ev.currentTarget.id);
+  }
+
   render() {
     const data = [
       {
         color: 'steelblue',
-        points: [{x: 1, y: 2}, {x: 3, y: 5}, {x: 7, y: -3}, {x: 9, y: -4},
-                  {x: 11, y: 0}, {x: 12, y: -1}]
+        label: 'ConcurrentUser',
+        points: [{x: 2, y: 4}, {x: 3, y: 6}, {x: 7, y: 1}, {x: 9, y: 0},
+                  {x: 11, y: 2}, {x: 12, y: 1}]
       }
     ];
 
     const data2 = [
       {
         color: 'green',
-        points: [{x: 2, y: 2}, {x: 4, y: 5}, {x: 9, y: -3}, {x: 11, y: -4},
-                  {x: 13, y: 0}, {x: 15, y: -1}]
+        points: [{x: 2, y: 3}, {x: 4, y: 7}, {x: 9, y: 3}, {x: 11, y: 2},
+                  {x: 13, y: 2}, {x: 15, y: 3}]
       }
     ];
 
+    const bardata = [
+      {text: 'core1', value: 340},
+      {text: 'core2', value: 280},
+      {text: 'core3', value: 200},
+      {text: 'core4', value: 360},
+      {text: 'core5', value: 400},
+      {text: 'core6', value: 420},
+      {text: 'core7', value: 300},
+      {text: 'core8', value: 450}
+    ];
+    const margin = {top: 20, right: 20, bottom: 30, left: 40};
+
     return (
-      <div className="nodes-resources" style={{display: 'flex', justifyContent: 'center'}}>
+      <div className="nodes-resources" style={{ display: 'flex', justifyContent: 'center', position: 'relative', top: 200, left: 1 }}>
         <Logo transform="translate(24,24) scale(0.25)" />
-        <h2>
+        <div style={{width: '20%', height: '%15'}}>
           <LineChart
-            width={640}
-            height={480}
+            title={'Concurrent User'}
+            width={600}
+            height={400}
             data={data}
           />
           <LineChart
-            width={640}
-            height={480}
+            width={600}
+            height={400}
             data={data2}
           />
-        </h2>
+        </div>
+        <div style={{width: '20%', height: '%15', display: 'flex', justifyContent: 'center', position: 'relative', top: 80, left: 280}}>
+          <BarChart
+            title={'Usage per Core'}
+            data={bardata}
+            width={480}
+            height={340}
+            margin={margin}
+            onClick={this.handleMouseEnter}
+           />
+        </div>
       </div>
     );
   }
--- app/scripts/components/troubleshooting-menu.js
+++ app/scripts/components/troubleshooting-menu.js
@@ -10,87 +10,50 @@ import {
 class DebugMenu extends React.Component {
   constructor(props, context) {
     super(props, context);
-    this.state = {value: ''};
-
-    this.handleClickReset = this.handleClickReset.bind(this);
-    this.handleClickPost = this.handleClickPost.bind(this);
+    this.state = { value: 'Initial data...'};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleClickReset(ev) {
-    ev.preventDefault();
-    this.props.resetLocalViewState();
-  }
-
-  handleClickPost(ev) {
-    // alert('Post request was submitted: ');
-    // const dataStr = 'cpu_load_short,host=server02,region=us-west value=0.55 1422568543702900257';
-    const blob = new Blob(['cpu_load_short,host=server03,region=us-west value=0.56 1422568543702900258'], {type: 'text/plain'});
-    fetch('http://10.145.240.148:8086/write?db=mydb&u=root&p=root', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: blob
-//      body: JSON.stringify({
-//        firstParam: this.state.value,
-//        secondParam: 'yourOtherValue',
-//      })
+  reset() {
+    // ev.preventDefault();
+    // this.props.resetLocalViewState();
+    this.setState({
+      value: 'dpdk'
     });
+  }
 
+  alertValue() {
+    alert(this.state.value);
+  }
 
-    console.log(this.state.value);
+  handleChange(ev) {
+    this.setState({value: ev.target.value});
+  }
+
+  handleSubmit(ev) {
+    alert(this.state.value);
     ev.preventDefault();
-    // this.props.resetLocalViewState();
-    // this.props.resetLocalViewState();
   }
 
   render() {
-    const reportDownloadUrl = process.env.WEAVE_CLOUD
-      ? `${window.location.origin}/api${window.location.pathname}/api/report`
-      : 'api/report';
     return (
       <div className="troubleshooting-menu-wrapper">
         <div className="troubleshooting-menu-content">
           <h3>Network Control & Monitor</h3>
-          <div className="troubleshooting-menu-item">
-            <tr>
-              <td>
-                <input type="text" value="dpdk|  " />
-              </td>
-              <td>
-                <input type="text" value="Monitor Server" />
-              </td>
-            </tr>
+          <h4>{new Date().toLocaleTimeString()}</h4>
+          <h4>{this.props.name}</h4>
+          <div className="troubleshooting-menu-item" style={{height: 360, width: 480}}>
             <h4>Post Request</h4>
-            <tr>
-              <td>
-                <a
-                  className="footer-icon"
-                  href={reportDownloadUrl}
-                  onClick={this.handleClickPost}
-                  title="Post Request to Monitor Server"
-                >
-                  <span className="fa fa-external-link" />
-                  <span className="description">
-                    Submit
-                  </span>
-                </a>
-              </td>
-              <td>
-                <a
-                  className="footer-icon"
-                  href={reportDownloadUrl}
-                  onClick={this.handleClickReset}
-                  title="Reset your local view state and reload the page"
-                >
-                  <span className="fa fa-undo" />
-                  <span className="description">
-                    Reset
-                  </span>
-                </a>
-              </td>
-            </tr>
+            <form onSubmit={this.handleSubmit}>
+              <label htmlFor="flavor">
+                Pick your favorite La Croix flavor:
+              </label>
+              <br />
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+              <br />
+              <input type="submit" value="Submit" />
+            </form>
           </div>
         </div>
       </div>
