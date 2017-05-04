548eff89839a926edec8e0d29efb56ae55a57665
Hua Zhang <hua.zhang1@huawei.com>
2017-05-04 14:13:49 -0700

    sudo curl -L git.io/scope -o /usr/local/bin/scope sudo chmod a+x /usr/local/bin/scope sudo scope launch 192.168.56.101 192.168.56.102

--- scope-launch.sh
+++ scope-launch.sh
@@ -2,4 +2,4 @@
 
 sudo curl -L git.io/scope -o /usr/local/bin/scope
 sudo chmod a+x /usr/local/bin/scope
-sudo scope launch 10.145.240.137 10.145.240.104
+sudo scope launch 192.168.56.101 192.168.56.102
